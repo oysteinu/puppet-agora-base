c69a4c0d69fe2d70ccaf81bebba9625c3749928b
Øystein Ulseth <ulseth@gmail.com>
2015-08-30 07:40:21 +0200

    Add  utf8 config for MySql

--- manifests/sql.pp
+++ manifests/sql.pp
@@ -2,4 +2,15 @@ class agorabase::sql {
 	class { "mysql":
     	root_password => 'auto',
   	}
+
+  	file 
+    { 
+        "/etc/mysql/conf.d/utf8_charset.cnf":
+            ensure  => present,
+            owner => root, group => root,
+            mode => '0775',
+            notify  => Service['mysql'],
+            source  => "puppet:///modules/agorabase/utf8_charset.cnf",
+            require => Class["mysql"],
+    }
 }
