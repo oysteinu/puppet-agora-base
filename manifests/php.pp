c4a43436a039f8c2918a46e436c6931b2a22aca2
Øystein Ulseth <ulseth@gmail.com>
2016-12-11 12:27:22 +0100

    NA

--- manifests/php.pp
+++ manifests/php.pp
@@ -30,9 +30,6 @@ class agorabase::php
       phpunit      => false,
 
       extensions => {
-        cli   => { },
-        mysql => { },
-        pear  => { },
         curl  => { },
       },
     }
