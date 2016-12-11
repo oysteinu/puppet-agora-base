02538a8f89bce6e11e3f41d45a4bc8bd9baa4c23
Øystein Ulseth <ulseth@gmail.com>
2016-12-11 08:40:33 +0100

    NA

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,4 @@ class agorabase {
 	include agorabase::apache
 	include agorabase::sql
 	include agorabase::phpmyadmin
-
-	class { 'composer':
-  		auto_update => true,
-  		require => [ Class['agorabase::php'] ],
-	}
 }
