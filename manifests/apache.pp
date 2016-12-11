3067dcf1e566e9d8738b3b9fd9f11656e016902e
Øystein Ulseth <ulseth@gmail.com>
2016-12-11 09:10:49 +0100

    no message

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -4,7 +4,7 @@ class agorabase::apache
     {
         "apache2":
             ensure  => present,
-            require => [Exec['apt-get update'], Package['::php']]
+            require => [Exec['apt-get update']]
     }
 
     service
