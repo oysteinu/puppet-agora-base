331d3c91d5394193b4c5f2f2a4d6a2fc5c9c3933
Øystein Ulseth <ulseth@gmail.com>
2016-12-12 20:57:37 +0100

    Downgraded to PHP version 5.6

--- manifests/php.pp
+++ manifests/php.pp
@@ -20,8 +20,10 @@ class agorabase::php
         "php5-xsl"
     ]
 
+    class { '::php::globals':
+      php_version => '5.6',
+    }->
     class { '::php':
-      ensure       => latest,
       manage_repos => true,
       fpm          => true,
       dev          => true,
