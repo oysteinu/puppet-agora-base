8f76c86f6e9b8923aa318aeb78dd62c32ddd8443
Øystein Ulseth <ulseth@gmail.com>
2017-01-04 21:46:19 +0100

    Cleaned up PHP config

--- manifests/php.pp
+++ manifests/php.pp
@@ -1,25 +1,5 @@
 class agorabase::php
 {
-    $packages = [
-        "php5",
-        "php5-cli",
-        "php5-mysql",
-        "php-pear",
-        "php5-dev",
-        "php-apc",
-        "php5-mcrypt",
-        "php5-gd",
-        "php5-imagick",
-        "php5-curl",
-        "libapache2-mod-php5",
-        "php5-xdebug",
-        "php5-memcache",
-        "php5-memcached",
-        "php5-pgsql",
-        "php5-sqlite",
-        "php5-xsl"
-    ]
-
     class { '::php':
       ensure       => latest,
       manage_repos => true,
