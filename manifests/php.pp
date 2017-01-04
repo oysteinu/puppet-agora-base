4ea84965b213e5480ef49a8c40f2a2d195f9a017
Øystein Ulseth <ulseth@gmail.com>
2017-01-04 23:11:42 +0100

    Setting PHP-extensions manually

--- manifests/php.pp
+++ manifests/php.pp
@@ -8,11 +8,25 @@ class agorabase::php
       composer     => true,
       pear         => true,
       phpunit      => false,
+    }
 
-      extensions => {
-        curl  => { },
-        xsl => { },
-        mcrypt => { },
-      },
+    package { 'php-curl':
+      ensure => 'latest',
+      require => Class['::php'],
+    }
+
+    package { 'php-mbstring':
+      ensure => 'latest',
+      require => Class['::php'],
+    }
+
+    package { 'php-xsl':
+      ensure => 'latest',
+      require => Class['::php'],
+    }
+
+    package { 'php-mcrypt':
+      ensure => 'latest',
+      require => Class['::php'],
     }
 }
