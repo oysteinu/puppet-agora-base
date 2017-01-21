619b199270b3b4ffb9b1fa1889a8c424334c12d1
Øystein Ulseth <ulseth@gmail.com>
2017-01-21 14:53:07 +0100

    NA

--- manifests/php.pp
+++ manifests/php.pp
@@ -1,15 +1,11 @@
 class agorabase::php
 {
-    class { '::php::globals':
-      php_version => '7.0',
-      config_root => '/etc/php/7.0',
-    }->
     class { '::php':
       ensure       => latest,
       manage_repos => true,
-      fpm          => false,
+      fpm          => true,
       dev          => false,
-      composer     => false,
+      composer     => true,
       pear         => true,
       phpunit      => false,
 
